3c59d21b49202886e580fa73d4c02d48b185cb81
Tom E Flaherty <teflaherty@utexas.edu>
2023-07-23 17:22:29 -0500

    Add project files.

---
+++ DemolishToPhase/Utils/Utils.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemolishToPhase
+{
+    internal static class Utils
+    {
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel currentPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            if (currentPanel == null)
+                currentPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            return currentPanel;
+        }
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
+        }
+
+        internal static int GetPhaseIndex(PhaseArray phaseArray, Phase targetPhase)
+        {
+            int phaseIndex = -1;
+            if (phaseArray != null && targetPhase != null)
+            {
+                PhaseArrayIterator iterator = phaseArray.ForwardIterator();
+                int index = 0;
+                while (iterator.MoveNext())
+                {
+                    Phase phase = iterator.Current as Phase;
+                    if (phase.Id == targetPhase.Id)
+                    {
+                        phaseIndex = index;
+                        break;
+                    }
+                    index++;
+                }
+            }
+            return phaseIndex;
+        }
+    }
+}
---
+++ DemolishToPhase/cmdDemolishToPhase.cs
@@ -0,0 +1,53 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace DemolishToPhase
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdDemolishToPhase : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // put any code needed for the form here
+
+            // open form
+            MyForm currentForm = new MyForm()
+            {
+                Width = 800,
+                Height = 450,
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+                Topmost = true,
+            };
+
+            currentForm.ShowDialog();
+
+            // get form data and do something
+
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
