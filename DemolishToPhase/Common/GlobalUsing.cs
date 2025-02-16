e7394aae2871be271caa3bb2a2ca22b7dc92f3d9
Tom E Flaherty <teflaherty@utexas.edu>
2025-02-16 10:53:04 -0600

    2 errors

--- DemolishToPhase/Common/GlobalUsing.cs
+++ DemolishToPhase/Common/GlobalUsing.cs
@@ -8,6 +8,7 @@ global using Autodesk.Revit.DB;
 global using Autodesk.Revit.UI;
 global using Color = Autodesk.Revit.DB.Color;
 global using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+global using TaskDialogIcon = Autodesk.Revit.UI.TaskDialogIcon;
 global using View = Autodesk.Revit.DB.View;
 global using RadioButton = System.Windows.Controls.RadioButton;
 
