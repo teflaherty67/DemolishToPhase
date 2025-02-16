7e1babf1f930f70d5e02048333b790b2b5fcd2e9
Tom E Flaherty <teflaherty@utexas.edu>
2025-02-15 18:28:27 -0600

    Add project files.

---
+++ DemolishToPhase/Common/Utils.cs
@@ -0,0 +1,187 @@
+﻿using DemolishToPhase.Forms;
+
+namespace DemolishToPhase
+{
+    internal static class Utils
+    {
+        public static bool ShowForm;
+        internal static void Run(UIApplication uiapp, Document curDoc)
+        {
+            frmDemolishToPhase curWin = new frmDemolishToPhase(uiapp.ActiveUIDocument.Document);
+            curWin.WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
+            curWin.ShowDialog();
+
+            if (Utils.ShowForm)
+            {
+                IList<Reference> pickList = uiapp.ActiveUIDocument.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                using (Transaction t = new Transaction(curDoc))
+                {
+                    t.Start("Demolish to Phase");
+
+                    if (pickList.Count != 0)
+                    {
+                        foreach (Reference pick in pickList)
+                        {
+                            // get current element from the pick list
+                            Element curElem = curDoc.GetElement(pick);
+
+                            ElementId demoPhaseId = null;
+
+                            Parameter paramPhaseDemo = curElem.get_Parameter(BuiltInParameter.PHASE_DEMOLISHED);
+
+                            // get the PhaseDemolishedID
+                            if (curWin.selectedPhase == "None")
+                            {
+                                demoPhaseId = ElementId.InvalidElementId;
+                            }
+                            else if (curWin.selectedPhase != "None")
+                            {
+                                // get the value of the PHASE_CREATED parameter for curElem
+                                Parameter paramPhaseCreated = curElem.get_Parameter(BuiltInParameter.PHASE_CREATED);
+
+                                Phase createdPhase = curElem.Document.GetElement(paramPhaseCreated.AsElementId()) as Phase;
+
+                                // get the index of the PHASE_CREATED value from the phase array
+                                int indexCreated = GetPhaseIndex(curWin.arrayPhases, createdPhase);
+
+                                // get the value of the selectedDemoPhase variable
+                                Phase demoPhase = Utils.getPhaseByName(curDoc, curWin.selectedPhase);
+
+                                // get the index of the selectedDemoPhase variable from the phase array
+                                int indexDemo = GetPhaseIndex(curWin.arrayPhases, demoPhase);
+
+                                // if the index of selectedDemoPhase is less than the index of PHASE_CREATED warn the user
+                                if (indexDemo < indexCreated)
+                                {
+                                    TaskDialog tdIndex = new TaskDialog("Invalid");
+                                    tdIndex.MainIcon = TaskDialogIcon.TaskDialogIconWarning;
+                                    tdIndex.Title = "Phase to Demolish";
+                                    tdIndex.TitleAutoPrefix = false;
+                                    tdIndex.MainContent = "Invalid order of phases: an object cannot be demolished before it was created.";
+                                    tdIndex.CommonButtons = TaskDialogCommonButtons.Close;
+
+                                    TaskDialogResult tdIndexRes = tdIndex.Show();
+                                }
+
+                                // if the index is greater than, or equal to, the index of PHASE_CREATED
+                                else if (indexDemo >= indexCreated)
+                                {
+                                    // set the demoPhaseID = selectedDemoPhase.Id
+                                    demoPhaseId = demoPhase.Id;
+                                }
+                            }
+
+                            // set the Phase_Demolished parameter
+                            if (demoPhaseId != null)
+                            {
+                                paramPhaseDemo.Set(demoPhaseId);
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        TaskDialog tdNone = new TaskDialog("Invalid");
+                        tdNone.MainIcon = TaskDialogIcon.TaskDialogIconWarning;
+                        tdNone.Title = "Phase to Demolish";
+                        tdNone.TitleAutoPrefix = false;
+                        tdNone.MainContent = "No elements were selected. Select some elements and try again.";
+                        tdNone.CommonButtons = TaskDialogCommonButtons.Close;
+
+                        TaskDialogResult tdNoneRes = tdNone.Show();
+                    }
+
+                    t.Commit();
+                }
+            }
+        }
+
+        #region Phases
+
+        public static List<Element> getAllPhases(Document curDoc)
+        {
+            FilteredElementCollector curColl = new FilteredElementCollector(curDoc);
+            curColl.OfCategory(BuiltInCategory.OST_Phases);
+
+            return curColl.OrderBy(x => x.Name).ToList();
+        }
+
+        public static List<Element> getAllPhaseFilters(Document curDoc)
+        {
+            FilteredElementCollector curColl = new FilteredElementCollector(curDoc);
+            curColl.OfClass(typeof(PhaseFilter));
+
+            return curColl.OrderBy(x => x.Name).ToList();
+        }
+
+        public static Phase getPhaseByName(Document curDoc, string phaseName)
+        {
+            // get all phases
+            List<Element> phaseList = getAllPhases(curDoc);
+
+            foreach (Phase curPhase in phaseList)
+            {
+                if (curPhase.Name == phaseName)
+                    return curPhase as Phase;
+            }
+
+            return null;
+
+        }
+
+        internal static int GetPhaseIndex(PhaseArray phaseArray, Phase targetPhase)
+        {
+            int phaseIndex = -1;
+
+            if (phaseArray != null && targetPhase != null)
+            {
+                PhaseArrayIterator m_iterator = phaseArray.ForwardIterator();
+                int m_index = 0;
+
+                while (m_iterator.MoveNext())
+                {
+                    Phase curPhase = m_iterator.Current as Phase;
+
+                    if (curPhase.Id == targetPhase.Id)
+                    {
+                        phaseIndex = m_index;
+                        break;
+                    }
+
+                    m_index++;
+                }
+            }
+
+            return phaseIndex;
+        }
+
+
+        #endregion
+
+        #region Ribbon
+
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel currentPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            if (currentPanel == null)
+                currentPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            return currentPanel;
+        }
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ DemolishToPhase/Forms/frmDemolishToPhase.xaml.cs
@@ -0,0 +1,77 @@
+﻿using DemolishToPhase.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemolishToPhase.Forms
+{
+    /// <summary>
+    /// Interaction logic for frmDemolishToPhase.xaml
+    /// </summary>
+    public partial class frmDemolishToPhase : Window
+    {
+        public PhaseArray arrayPhases;
+
+        public string selectedPhase;
+
+        public frmDemolishToPhase(Document curDoc)
+        {
+            // get all the phases in the project
+            arrayPhases = curDoc.Phases;
+
+            // create an empty list to hold the phase names
+            List<string> phases = new List<string>();
+
+            phases.Add("None");
+
+            // loop through the phases
+            foreach (Phase curPhase in arrayPhases)
+            {
+                // add the phase name to the list
+                phases.Add(curPhase.Name);
+            }
+
+            InitializeComponent();
+
+            foreach (string phase in phases)
+            {
+                RadioButton rb = new RadioButton() { Content = phase, Height = 25, Width = 120 };
+                sp.Children.Add(rb);
+                rb.Checked += new RoutedEventHandler(rb_Checked);
+                rb.Unchecked += new RoutedEventHandler(rb_Unchecked);
+            }
+        }
+
+        void rb_Unchecked(object sender, RoutedEventArgs e)
+        {
+            selectedPhase = "";
+        }
+
+        void rb_Checked(object sender, RoutedEventArgs e)
+        {
+            selectedPhase = (sender as RadioButton).Content.ToString();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.ShowForm = true;
+            this.Close();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.ShowForm = false;
+            this.Close();
+        }
+    }
+}
